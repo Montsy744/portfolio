302ab4469f5fecb14a60210539418fa28d6fecbb
Hugo Straseele <straseelehugo@gmail.com>
2025-06-17 17:59:33 +0200

    update

---
+++ JS/defilement.js
@@ -0,0 +1,13 @@
+document.querySelectorAll("#liens a").forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault(); // Empêche le comportement par défaut du lien
+
+        const targetId = this.getAttribute("href"); // Récupère l'ID de l'ancre
+        const targetElement = document.querySelector(targetId); // Sélectionne l'élément cible
+
+        // Défilement fluide vers l'élément cible
+        targetElement.scrollIntoView({
+        behavior: "smooth", // Effet de défilement fluide
+        });
+    });
+});
