b3d9509d3795956a34a04a1ace30a05f44332e06
Hugo Straseele <straseelehugo@gmail.com>
2026-01-20 11:24:34 +0100

    affichage des compétences via Javascript et non Html

---
+++ JS/comp.js
@@ -0,0 +1,60 @@
+const dataComp = [
+    {
+        name: "HTML",
+        image: "./../public/img/stack/html.png"
+    },
+    {
+        name: "CSS",
+        image: "./../public/img/stack/css.png"
+    },
+    {
+        name: "JavaScript",
+        image: "./../public/img/stack/JS.png"
+    },
+    {
+        name: "Java",
+        image: "./../public/img/stack/Java.png"
+    },
+    {
+        name: "SQL",
+        image: "./../public/img/stack/sql.png"
+    },
+    {
+        name: "git",
+        image: "./../public/img/stack/git.png"
+    },
+    {
+        name: "markdown",
+        image: "./../public/img/stack/md.png"
+    },
+    {
+        name: "python",
+        image: "./../public/img/stack/python.png"
+    },
+    {
+        name: "TypeScript",
+        image: "./../public/img/stack/TypeScript.png"
+    }
+    
+]
+
+let htmlComp = "";
+const divComp = document.querySelector(`#skilsContaineur`);
+
+const renderHtmlComp = ({name, image}) => {
+    return `<div class="skilsItem" id="${name}">
+                <div class="containerPosition">
+                    <div class="conatinerLogo">
+                        <img src="${image}" alt="logo ${name}" class="bg">
+                        <img src="${image}" alt="logo ${name}" class="devant">
+                    </div>
+                </div>
+                <p>${name}</p>
+            </div>`
+}
+
+dataComp.forEach((prop) => {
+    htmlComp += renderHtmlComp(prop);
+});
+
+divComp.innerHTML = htmlComp
