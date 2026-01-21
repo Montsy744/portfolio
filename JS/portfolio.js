6c5b4ef491728331e7619e86d871832483dfe0bf
Hugo Straseele <straseelehugo@gmail.com>
2026-01-21 19:58:40 +0100

    ajout des projet perso du modal 1 en inserssion depuis le js et non dans le html

--- JS/portfolio.js
+++ JS/portfolio.js
@@ -1,19 +1,182 @@
-const dataPortfolio = [
+const dataPortfolioPerso = [
+  {
+    imgProjet: "./../public/img/project/fondTodoListReact.png",
+    title: "Une TodoList React",
+    description:
+      "Une application de TodoList codée en TypeScript avec React et Tailwind CSS.",
+    lienCode: "https://github.com/Montsy744/A_react_todo_list",
+    lienSite: "https://a-react-todo-list.vercel.app/",
+    imgStack: [
+      "./../public/img/stack/typescript.png",
+      "./../public/img/framework/react.png",
+      "./../public/img/framework/vite.png",
+      "./../public/img/stack/css.png",
+      "./../public/img/stack/js.png",
+      "./../public/img/stack/git.png",
+    ],
+  },
+  {
+    imgProjet: "./../public/img/project/fondMarkdownEditeur.png",
+    title: "Editeur Markdown",
+    description:
+      "Un éditeur permettant de prévisualiser son code markdown en html. Ceci est mon premier projet avec react.",
+    lienCode: "https://github.com/Montsy744/EditeurMd",
+    lienSite: "https://montsy744.github.io/EditeurMd/",
+    imgStack: [
+      "./../public/img/framework/react.png",
+      "./../public/img/stack/css.png",
+      "./../public/img/stack/js.png",
+      "./../public/img/stack/git.png",
+    ],
+  },
+  {
+    imgProjet: "./../public/img/project/fondHerbergeInDHope.png",
+    title: "Herberge In d'Hope",
+    description:
+      "Un site pour un restaurant traditionnel de la région Hauts-de-France.",
+    lienCode: "https://github.com/Montsy744/Herberge_In_d_Hope",
+    lienSite: "https://montsy744.github.io/Herberge_In_d_Hope/",
+    imgStack: [
+      "./../public/img/stack/html.png",
+      "./../public/img/stack/css.png",
+      "./../public/img/stack/js.png",
+      "./../public/img/stack/git.png",
+    ],
+  },
   {
     imgProjet: "./../public/img/project/fondEdurhum.png",
     title: "Site du bureau des étudiants",
     description:
       "J'ai créé ce site pour le bureau des étudiants du BUT informatique de l'Université de Lille.",
-    lienCode: "https://montsy744.github.io/edurhum-site/",
-    lienSite: "https://github.com/Montsy744/edurhum-site",
+    lienCode: "https://github.com/Montsy744/edurhum-site",
+    lienSite: "https://montsy744.github.io/edurhum-site/",
     imgStack: [
       "./../public/img/stack/html.png",
       "./../public/img/stack/css.png",
-      "./../public/img/stack/JS.png",
+      "./../public/img/stack/js.png",
+      "./../public/img/stack/git.png",
+    ],
+  },
+  {
+    imgProjet: "./../public/img/project/fondJeuDeDevinette.png",
+    title: "Jeu de devinette de mot",
+    description:
+      "Un petit jeu de devinettes de mots codé en html, css et JS.",
+    lienCode: "https://github.com/Montsy744/jeuDeDevinette",
+    lienSite: "https://montsy744.github.io/jeuDeDevinette/",
+    imgStack: [
+      "./../public/img/stack/html.png",
+      "./../public/img/stack/css.png",
+      "./../public/img/stack/js.png",
+      "./../public/img/stack/git.png",
+    ],
+  },
+  {
+    imgProjet: "./../public/img/project/fondToDoList.png",
+    title: "TodoList",
+    description:
+      "Un petit site qui permet de faire une todo list très simple mais efficace.",
+    lienCode: "https://github.com/Montsy744/ToDoList",
+    lienSite: "https://montsy744.github.io/ToDoList/",
+    imgStack: [
+      "./../public/img/stack/html.png",
+      "./../public/img/stack/css.png",
+      "./../public/img/stack/js.png",
+      "./../public/img/stack/git.png",
+    ],
+  },
+  {
+    imgProjet: "./../public/img/project/fondJeuDeTire.png",
+    title: "Jeu de tir",
+    description:
+      "Un petit jeu de tir simple.",
+    lienCode: "https://github.com/Montsy744/jeuDeTire",
+    lienSite: "https://montsy744.github.io/jeuDeTire/",
+    imgStack: [
+      "./../public/img/stack/html.png",
+      "./../public/img/stack/css.png",
+      "./../public/img/stack/js.png",
+      "./../public/img/stack/git.png",
+    ],
+  },
+  {
+    imgProjet: "./../public/img/project/fonPasswordGenerator.png",
+    title: "Générateur de mot de passe",
+    description:
+      "Un site simple pour générer un mot de passe de 12 caractères.",
+    lienCode: "https://github.com/Montsy744/genratorPassword/",
+    lienSite: "https://montsy744.github.io/genratorPassword/",
+    imgStack: [
+      "./../public/img/stack/html.png",
+      "./../public/img/stack/css.png",
+      "./../public/img/stack/js.png",
+      "./../public/img/stack/git.png",
+    ],
+  },
+  {
+    imgProjet: "./../public/img/project/fondCalculatricePython.png",
+    title: "Calculatrice Python",
+    description:
+      "Une calculatrice codée en Python avec Tkinter.",
+    lienCode: "https://github.com/Montsy744/calculator.py",
+    lienSite: "https://montsy744.itch.io/calculatrice-tkinter",
+    imgStack: [
+      "./../public/img/stack/python.png",
+      "./../public/img/stack/git.png",
+    ],
+  },
+  {
+    imgProjet: "./../public/img/project/fondParticulesThreeJ.png",
+    title: "Nuages de particules",
+    description:
+      "Un nuage de particules qui m'a servi de base pour débuter threeJS.",
+    lienCode: "https://github.com/Montsy744/particuleThreeJS",
+    lienSite: "",
+    imgStack: [
+      "./../public/img/stack/html.png",
+      "./../public/img/stack/css.png",
+      "./../public/img/stack/js.png",
+      "./../public/img/stack/git.png",
+    ],
+  },
+  {
+    imgProjet: "./../public/img/project/loader.png",
+    title: "Loader",
+    description:
+      "Un loader créé en css dont la vidéo du codage est disponible sur mon Instagram.",
+    lienCode: "https://github.com/Montsy744/loader",
+    lienSite: "https://montsy744.github.io/loader/",
+    imgStack: [
+      "./../public/img/stack/html.png",
+      "./../public/img/stack/css.png",
+      "./../public/img/stack/js.png",
+      "./../public/img/stack/git.png",
+    ],
+  },
+  {
+    imgProjet: "./../public/img/project/fonLeanrCodeHub.png",
+    title: "LearnCodeHub",
+    description:
+      "Un site pour apprendre les langages de programmation comme HTML, CSS et autres. (non responsive)",
+    lienCode: "https://github.com/Montsy744/LearnCodeHub",
+    lienSite: "https://montsy744.github.io/LearnCodeHub/",
+    imgStack: [
+      "./../public/img/stack/html.png",
+      "./../public/img/stack/css.png",
       "./../public/img/stack/git.png",
     ],
   },
 ];
+
+const renderHtmlStack = (imgStack) => {
+  let htmlTemp = "";
+
+  imgStack.forEach((prop) => {
+    htmlTemp += `<img src="${prop}" alt="logo Html"  class="stack"></img>`
+  })
+
+  return htmlTemp;
+}
 
 const renderHtmlPortfolio = ({
   imgProjet,
@@ -23,7 +186,7 @@ const renderHtmlPortfolio = ({
   lienSite,
   imgStack,
 }) => {
-  const liensProjet = "";
+  let liensProjet = "";
 
   if (lienSite === ``) {
     liensProjet += `
@@ -53,8 +216,15 @@ const renderHtmlPortfolio = ({
         </div>`;
 };
 
+let htmlPersoOrdi = "";
+
 const projetPersoOrdi = document.querySelector(`#modalContainer`);
 const projetUnivOrdi = document.querySelector(`#modalContainerUnniversitaire`);
 
 const projetPersoTel = document.querySelector(`#modalContainerTelPerso`);
 const projetUnivTel = document.querySelector(`#modalContainerTelUniv`);
+
+dataPortfolioPerso.forEach((prop) => {
+  htmlPersoOrdi += renderHtmlPortfolio(prop);
+})
+projetPersoOrdi.innerHTML = htmlPersoOrdi;
