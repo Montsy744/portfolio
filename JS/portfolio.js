f91ec775345c6cf56e5a735bf27b0ed8ca944635
Hugo Straseele <straseelehugo@gmail.com>
2026-01-22 19:11:19 +0100

    ajout des data des projet de l'unniv dans le js

--- JS/portfolio.js
+++ JS/portfolio.js
@@ -7,8 +7,8 @@ const dataPortfolioPerso = [
     lienCode: "https://github.com/Montsy744/A_react_todo_list",
     lienSite: "https://a-react-todo-list.vercel.app/",
     imgStack: [
-       "./../public/img/stack/TypeScript.png",    
-      "./../public/img/framework/REACT.png",     
+      "./../public/img/stack/TypeScript.png",
+      "./../public/img/framework/REACT.png",
       "./../public/img/framework/vite.png",
       "./../public/img/stack/css.png",
       "./../public/img/stack/js.png",
@@ -132,6 +132,7 @@ const dataPortfolioPerso = [
     imgStack: [
       "./../public/img/stack/html.png",
       "./../public/img/stack/css.png",
+      "./../public/img/stack/threejs.png",
       "./../public/img/stack/js.png",
       "./../public/img/stack/git.png",
     ],
@@ -165,6 +166,120 @@ const dataPortfolioPerso = [
   },
 ];
 
+const dataPortfolioUniv = [
+  {
+    imgProjet: "./../public/img/project/unniv/fondCompilFighter.png",
+    title: "CompilFighter",
+    description:
+      "un jeu de culture générale a but ludo pédagogique a destination de collégiens. Mon premier projet en équipe",
+    lienCode: "https://github.com/Montsy744/CompilFighter",
+    lienSite: "",
+    imgStack: [
+      "./../public/img/stack/java.png",
+      "./../public/img/stack/git.png",
+    ],
+  },
+  {
+    imgProjet: "./../public/img/project/unniv/fondSpotify.png",
+    title: "Ride with Spotify",
+    description:
+      "Dans ce projet d’équipe, nous avons conçu et développé un site web responsive simulant une plateforme de covoiturage interne pour les employés de Spotify.",
+    lienCode: "https://github.com/Montsy744/SaeSpotifyWeb",
+    lienSite: "https://montsy744.github.io/SaeSpotifyWeb",
+    imgStack: [
+      "./../public/img/stack/html.png",
+      "./../public/img/stack/css.png",
+      "./../public/img/stack/git.png",
+    ],
+  },
+  {
+    imgProjet: "./../public/img/project/unniv/fondSae203.png",
+    title: "Installation VM",
+    description:
+      "Ce projet nous a permis de découvrir l’environnement Linux en configurant et sécurisant une machine virtuelle Debian.",
+    lienCode: "https://github.com/Montsy744/SAE2.03_Installtion_VM",
+    lienSite: "",
+    imgStack: [
+      "./../public/img/stack/linux.png",
+      "./../public/img/stack/md.png",
+      "./../public/img/stack/git.png",
+    ],
+  },
+  {
+    imgProjet: "./../public/img/project/unniv/fondBDDBut1.png",
+    title: "Utilisation BDD",
+    description:
+      "Projet d’initiation aux bases de données relationnelles avec Access et SQL, axé sur la modélisation, les requêtes et le travail en équipe.",
+    lienCode: "https://github.com/Montsy744/La_BDD_et_les_JO",
+    lienSite: "",
+    imgStack: [
+      "./../public/img/stack/sql.png",
+      "./../public/img/stack/git.png",
+    ],
+  },
+  {
+    imgProjet: "./../public/img/project/unniv/fondAdoMatch.png",
+    title: "AdoMatch",
+    description:
+      "Projet Java de gestion d’affectations dans un échange international, axé sur la modélisation, la POO, les algorithmes de tri et le travail en équipe.",
+    lienCode: "https://github.com/Montsy744/AdoMatch",
+    lienSite: "",
+    imgStack: [
+      "./../public/img/stack/java.png",
+      "./../public/img/stack/git.png",
+    ],
+  },
+  {
+    imgProjet: "./../public/img/project/unniv/saeVideoCom.png",
+    title: "HTML, CSS et JS : quel est leur rôle?",
+    description:
+      "Découvrez comment HTML, CSS et JavaScript donnent vie aux sites web modernes en une parfaite harmonie !",
+    lienCode: "video",
+    lienSite: "",
+    imgStack: [
+      "./../public/img/stack/html.png",
+      "./../public/img/stack/css.png",
+      "./../public/img/stack/git.png",
+    ],
+  },
+  {
+    imgProjet: "./../public/img/project/unniv/fondGoldenAce.png",
+    title: "Golden Ace",
+    description:
+      "un jeu de simulation de casino coder en java en équipe de 6 développeurs.",
+    lienCode: "https://github.com/Montsy744/GoldenAce",
+    lienSite: "",
+    imgStack: [
+      "./../public/img/stack/java.png",
+      "./../public/img/stack/git.png",
+    ],
+  },
+  {
+    imgProjet: "./../public/img/project/unniv/fondRTL.png",
+    title: "Rule The Labyrinth",
+    description:
+      "un jeu de labyrinthe coder en java/javaFx en équipe de 4 développeurs en suivant l'architecture MVC",
+    lienCode: "https://github.com/Montsy744/Rule_The_Labyrinth",
+    lienSite: "",
+    imgStack: [
+      "./../public/img/stack/java.png",
+      "./../public/img/stack/git.png",
+    ],
+  },
+  {
+    imgProjet: "./../public/img/project/unniv/fondShellC.png",
+    title: "Shell",
+    description:
+      "Implémentation d’un mini-shell en langage C proposant une invite interactive et l’exécution de commandes externes. Il sert d’exercice pratique pour comprendre les forks",
+    lienCode: "https://github.com/Montsy744/MyPersonalShell",
+    lienSite: "",
+    imgStack: [
+      "./../public/img/stack/c.png",
+      "./../public/img/stack/git.png",
+    ],
+  },
+]
+
 const renderHtmlStack = (imgStack) => {
   let htmlTemp = "";
 
@@ -175,18 +290,15 @@ const renderHtmlStack = (imgStack) => {
   return htmlTemp;
 };
 
-const renderHtmlPortfolio = ({
-  imgProjet,
-  title,
-  description,
-  lienCode,
-  lienSite,
-  imgStack,
-}) => {
+const liensProjetFunc = (lienCode, lienSite) => {
   let liensProjet = "";
 
-  if (lienSite === ``) {
+  if (lienCode === `video`) {
     liensProjet += `
+      <a href="" class="btnCard code" id="video">Voir la vidéo</a>
+    `;
+  } else if (lienSite === ``) {
+      liensProjet += `
         <a href="${lienCode}" target="_blank" class="btnCard code">Voir le code</a>
     `;
   } else {
@@ -196,9 +308,22 @@ const renderHtmlPortfolio = ({
     `;
   }
 
+  return liensProjet;
+};
+
+const renderHtmlPortfolio = ({
+  imgProjet,
+  title,
+  description,
+  lienCode,
+  lienSite,
+  imgStack,
+}) => {
+  const liensProjetPersoOrdi = liensProjetFunc(lienCode, lienSite);
+
   return `<div class="card">
             <div class="imgContainer">
-                <img src="${imgProjet}" alt="Image du lancement du site" class="card__image">
+                <img src="${imgProjet}" alt="Image du lancement du site" class="card__image imgProjet">
                 <div>
                     ${renderHtmlStack(imgStack)}
                 </div>
@@ -207,13 +332,45 @@ const renderHtmlPortfolio = ({
                 <p class="card__title">${title}</p>
                 <p class="card__description">${description}</p>
                 <div class="btnContainer">
-                    ${liensProjet}
+                    ${liensProjetPersoOrdi}
                 </div>
             </div>
         </div>`;
 };
 
+const renderHtmlPortfolioTel = ({
+  imgProjet,
+  title,
+  description,
+  lienCode,
+  lienSite,
+  imgStack,
+}) => {
+  let liensProjetPersoTel = liensProjetFunc(lienCode, lienSite);
+
+  return `<div class="cardTel">
+            <div class="imgContainerTel">
+              <img src="${imgProjet}" alt="Image du lancement du site" class="card__image">
+              <div>
+                ${renderHtmlStack(imgStack)}
+              </div>
+            </div>
+                        
+            <div class="card__content_tel">
+              <p class="card__title_tel">${title}</p>
+              <p class="card__description_tel">${description}</p>
+              <div class="btnContainer">
+                  ${liensProjetPersoTel}
+              </div>
+            </div>
+          </div>`;
+};
+
 let htmlPersoOrdi = "";
+let htmlPersoTel = "";
+
+let htmlUnivOrdi = "";
+let htmlUnivTel = "";
 
 const projetPersoOrdi = document.querySelector(`#modalContainer`);
 const projetUnivOrdi = document.querySelector(`#modalContainerUnniversitaire`);
@@ -223,5 +380,15 @@ const projetUnivTel = document.querySelector(`#modalContainerTelUniv`);
 
 dataPortfolioPerso.forEach((prop) => {
   htmlPersoOrdi += renderHtmlPortfolio(prop);
+  htmlPersoTel += renderHtmlPortfolioTel(prop);
 });
 projetPersoOrdi.innerHTML = htmlPersoOrdi;
+projetPersoTel.innerHTML = htmlPersoTel;
+
+dataPortfolioUniv.forEach((prop) => {
+  htmlUnivOrdi += renderHtmlPortfolio(prop);
+  htmlUnivTel += renderHtmlPortfolioTel(prop);
+})
+projetUnivOrdi.innerHTML = htmlUnivOrdi;
+projetUnivTel.innerHTML = htmlUnivTel;
+
