8b6e5ec5550735e4e5bdafcce29e48cd533a08e7
Hugo Straseele <straseelehugo@gmail.com>
2026-03-31 12:52:55 +0200

    ajout du css de la vue detail project

--- JS/portfolio.js
+++ JS/portfolio.js
@@ -1,348 +1,10 @@
-const dataPortfolioPerso = [
-  {
-    imgProjet: "./../public/img/project/fondTodoListReact.png",
-    title: "Une TodoList React",
-    description:
-      "Une application de TodoList codée en TypeScript avec React et Tailwind CSS.",
-    lienCode: "https://github.com/Montsy744/A_react_todo_list",
-    lienSite: "https://a-react-todo-list.vercel.app/",
-    imgStack: [
-      "./../public/img/stack/TypeScript.png",
-      "./../public/img/framework/REACT.png",
-      "./../public/img/framework/vite.png",
-      "./../public/img/stack/tailwindCss.png",
-      "./../public/img/stack/js.png",
-      "./../public/img/stack/git.png",
-    ],
-  },
-  {
-    imgProjet: "./../public/img/project/fondMarkdownEditeur.png",
-    title: "Editeur Markdown",
-    description:
-      "Un éditeur permettant de prévisualiser son code markdown en html. Ceci est mon premier projet avec react.",
-    lienCode: "https://github.com/Montsy744/EditeurMd",
-    lienSite: "https://montsy744.github.io/EditeurMd/",
-    imgStack: [
-      "./../public/img/framework/REACT.png",
-      "./../public/img/stack/css.png",
-      "./../public/img/stack/js.png",
-      "./../public/img/stack/git.png",
-    ],
-  },
-  {
-    imgProjet: "./../public/img/project/fondGSAPMojito.png",
-    title: "Mojito Landing Page",
-    description:
-      "Landing page animée pour un bar à cocktails fictif, réalisée avec React et GSAP afin de découvrir les animations avancées et améliorer l’expérience utilisateur.",
-    lienCode: "https://github.com/Montsy744/GSAP_Mojito_landingPage",
-    lienSite: "https://gsap-mojito-landing-page-orpin.vercel.app/",
-    imgStack: [
-      "./../public/img/framework/REACT.png",
-      "./../public/img/framework/vite.png",
-      "./../public/img/stack/tailwindCss.png",
-      "./../public/img/stack/css.png",
-      "./../public/img/stack/js.png",
-      "./../public/img/stack/git.png",
-    ],
-  },
-  {
-    imgProjet: "./../public/img/project/fondHerbergeInDHopeV2.png",
-    title: "Herberge In d'Hope refont du style",
-    description:
-      "Refonte du site que j'avais déja produit pour un restaurant notament avec un passage des technos vers React et TailwindCSS",
-    lienCode: "https://github.com/Montsy744/Herberge_In_d_Hope",
-    lienSite: "https://herberge-in-d-hope.vercel.app/",
-    imgStack: [
-      "./../public/img/framework/REACT.png",
-      "./../public/img/framework/vite.png",
-      "./../public/img/stack/tailwindCss.png",
-      "./../public/img/stack/css.png",
-      "./../public/img/stack/js.png",
-      "./../public/img/stack/git.png",
-    ],
-  },
-  {
-    imgProjet: "./../public/img/project/fondHerbergeInDHope.png",
-    title: "Herberge In d'Hope",
-    description:
-      "Un site pour un restaurant traditionnel de la région Hauts-de-France.",
-    lienCode: "https://github.com/Montsy744/Herberge_In_d_Hope",
-    lienSite: "https://montsy744.github.io/Herberge_In_d_Hope/",
-    imgStack: [
-      "./../public/img/stack/html.png",
-      "./../public/img/stack/css.png",
-      "./../public/img/stack/js.png",
-      "./../public/img/stack/git.png",
-    ],
-  },
-  {
-    imgProjet: "./../public/img/project/fondEdurhum.png",
-    title: "Site du bureau des étudiants",
-    description:
-      "J'ai créé ce site pour le bureau des étudiants du BUT informatique de l'Université de Lille.",
-    lienCode: "https://github.com/Montsy744/edurhum-site",
-    lienSite: "https://montsy744.github.io/edurhum-site/",
-    imgStack: [
-      "./../public/img/stack/html.png",
-      "./../public/img/stack/css.png",
-      "./../public/img/stack/js.png",
-      "./../public/img/stack/git.png",
-    ],
-  },
-  {
-    imgProjet: "./../public/img/project/fondJeuDeDevinette.png",
-    title: "Jeu de devinette de mot",
-    description: "Un petit jeu de devinettes de mots codé en html, css et JS.",
-    lienCode: "https://github.com/Montsy744/jeuDeDevinette",
-    lienSite: "https://montsy744.github.io/jeuDeDevinette/",
-    imgStack: [
-      "./../public/img/stack/html.png",
-      "./../public/img/stack/css.png",
-      "./../public/img/stack/js.png",
-      "./../public/img/stack/git.png",
-    ],
-  },
-  {
-    imgProjet: "./../public/img/project/fondToDoList.png",
-    title: "TodoList",
-    description:
-      "Un petit site qui permet de faire une todo list très simple mais efficace.",
-    lienCode: "https://github.com/Montsy744/ToDoList",
-    lienSite: "https://montsy744.github.io/ToDoList/",
-    imgStack: [
-      "./../public/img/stack/html.png",
-      "./../public/img/stack/css.png",
-      "./../public/img/stack/js.png",
-      "./../public/img/stack/git.png",
-    ],
-  },
-  {
-    imgProjet: "./../public/img/project/fondJeuDeTire.png",
-    title: "Jeu de tir",
-    description: "Un petit jeu de tir simple.",
-    lienCode: "https://github.com/Montsy744/jeuDeTire",
-    lienSite: "https://montsy744.github.io/jeuDeTire/",
-    imgStack: [
-      "./../public/img/stack/html.png",
-      "./../public/img/stack/css.png",
-      "./../public/img/stack/js.png",
-      "./../public/img/stack/git.png",
-    ],
-  },
-  {
-    imgProjet: "./../public/img/project/fonPasswordGenerator.png",
-    title: "Générateur de mot de passe",
-    description:
-      "Un site simple pour générer un mot de passe de 12 caractères.",
-    lienCode: "https://github.com/Montsy744/genratorPassword/",
-    lienSite: "https://montsy744.github.io/genratorPassword/",
-    imgStack: [
-      "./../public/img/stack/html.png",
-      "./../public/img/stack/css.png",
-      "./../public/img/stack/js.png",
-      "./../public/img/stack/git.png",
-    ],
-  },
-  {
-    imgProjet: "./../public/img/project/fondCalculatricePython.png",
-    title: "Calculatrice Python",
-    description: "Une calculatrice codée en Python avec Tkinter.",
-    lienCode: "https://github.com/Montsy744/calculator.py",
-    lienSite: "https://montsy744.itch.io/calculatrice-tkinter",
-    imgStack: [
-      "./../public/img/stack/python.png",
-      "./../public/img/stack/git.png",
-    ],
-  },
-  {
-    imgProjet: "./../public/img/project/fondParticulesThreeJ.png",
-    title: "Nuages de particules",
-    description:
-      "Un nuage de particules qui m'a servi de base pour débuter threeJS.",
-    lienCode: "https://github.com/Montsy744/particuleThreeJS",
-    lienSite: "",
-    imgStack: [
-      "./../public/img/stack/html.png",
-      "./../public/img/stack/css.png",
-      "./../public/img/stack/threejs.png",
-      "./../public/img/stack/js.png",
-      "./../public/img/stack/git.png",
-    ],
-  },
-  {
-    imgProjet: "./../public/img/project/loader.png",
-    title: "Loader",
-    description:
-      "Un loader créé en css dont la vidéo du codage est disponible sur mon Instagram.",
-    lienCode: "https://github.com/Montsy744/loader",
-    lienSite: "https://montsy744.github.io/loader/",
-    imgStack: [
-      "./../public/img/stack/html.png",
-      "./../public/img/stack/css.png",
-      "./../public/img/stack/js.png",
-      "./../public/img/stack/git.png",
-    ],
-  },
-];
+import { dataPortfolioPerso, dataPortfolioUniv } from "./portfolioProjet.js";
 
-const dataPortfolioUniv = [
-  {
-    imgProjet: "./../public/img/project/unniv/fondCompilFighter.png",
-    title: "CompilFighter",
-    description:
-      "un jeu de culture générale a but ludo pédagogique a destination de collégiens. Mon premier projet en équipe",
-    lienCode: "https://github.com/Montsy744/CompilFighter",
-    lienSite: "",
-    imgStack: [
-      "./../public/img/stack/java.png",
-      "./../public/img/stack/git.png",
-    ],
-  },
-  {
-    imgProjet: "./../public/img/project/unniv/fondSpotify.png",
-    title: "Ride with Spotify",
-    description:
-      "Dans ce projet d’équipe, nous avons conçu et développé un site web responsive simulant une plateforme de covoiturage interne pour les employés de Spotify.",
-    lienCode: "https://github.com/Montsy744/SaeSpotifyWeb",
-    lienSite: "https://montsy744.github.io/SaeSpotifyWeb",
-    imgStack: [
-      "./../public/img/stack/html.png",
-      "./../public/img/stack/css.png",
-      "./../public/img/stack/git.png",
-    ],
-  },
-  {
-    imgProjet: "./../public/img/project/unniv/fondSae203.png",
-    title: "Installation VM",
-    description:
-      "Ce projet nous a permis de découvrir l’environnement Linux en configurant et sécurisant une machine virtuelle Debian.",
-    lienCode: "https://github.com/Montsy744/SAE2.03_Installtion_VM",
-    lienSite: "",
-    imgStack: [
-      "./../public/img/stack/linux.png",
-      "./../public/img/stack/md.png",
-      "./../public/img/stack/git.png",
-    ],
-  },
-  {
-    imgProjet: "./../public/img/project/unniv/fondBDDBut1.png",
-    title: "Utilisation BDD",
-    description:
-      "Projet d’initiation aux bases de données relationnelles avec Access et SQL, axé sur la modélisation, les requêtes et le travail en équipe.",
-    lienCode: "https://github.com/Montsy744/La_BDD_et_les_JO",
-    lienSite: "",
-    imgStack: [
-      "./../public/img/stack/sql.png",
-      "./../public/img/stack/git.png",
-    ],
-  },
-  {
-    imgProjet: "./../public/img/project/unniv/fondAdoMatch.png",
-    title: "AdoMatch",
-    description:
-      "Projet Java de gestion d’affectations dans un échange international, axé sur la modélisation, la POO, les algorithmes de tri et le travail en équipe.",
-    lienCode: "https://github.com/Montsy744/AdoMatch",
-    lienSite: "",
-    imgStack: [
-      "./../public/img/stack/java.png",
-      "./../public/img/stack/git.png",
-    ],
-  },
-  {
-    imgProjet: "./../public/img/project/unniv/saeVideoCom.png",
-    title: "HTML, CSS et JS : quel est leur rôle?",
-    description:
-      "Découvrez comment HTML, CSS et JavaScript donnent vie aux sites web modernes en une parfaite harmonie !",
-    lienCode: "video",
-    lienSite: "",
-    imgStack: [
-      "./../public/img/stack/html.png",
-      "./../public/img/stack/css.png",
-      "./../public/img/stack/git.png",
-    ],
-  },
-  {
-    imgProjet: "./../public/img/project/unniv/fondGoldenAce.png",
-    title: "Golden Ace",
-    description:
-      "un jeu de simulation de casino coder en java en équipe de 6 développeurs.",
-    lienCode: "https://github.com/Montsy744/GoldenAce",
-    lienSite: "",
-    imgStack: [
-      "./../public/img/stack/java.png",
-      "./../public/img/stack/git.png",
-    ],
-  },
-  {
-    imgProjet: "./../public/img/project/unniv/fondRTL.png",
-    title: "Rule The Labyrinth",
-    description:
-      "un jeu de labyrinthe coder en java/javaFx en équipe de 4 développeurs en suivant l'architecture MVC",
-    lienCode: "https://github.com/Montsy744/Rule_The_Labyrinth",
-    lienSite: "",
-    imgStack: [
-      "./../public/img/stack/java.png",
-      "./../public/img/stack/git.png",
-    ],
-  },
-  {
-    imgProjet: "./../public/img/project/unniv/fondShellC.png",
-    title: "Shell",
-    description:
-      "Implémentation d’un mini-shell en langage C proposant une invite interactive et l’exécution de commandes externes. Il sert d’exercice pratique pour comprendre les forks",
-    lienCode: "https://github.com/Montsy744/MyPersonalShell",
-    lienSite: "",
-    imgStack: ["./../public/img/stack/c.png", "./../public/img/stack/git.png"],
-  },
-  {
-    imgProjet: "./../public/img/project/unniv/FondJSteam.png",
-    title: "JSteam",
-    description:
-      "Création d'une copie de Steam dans le but de découvrir les langages Javascript et Typescript en suivant des TP",
-    lienCode: "https://github.com/Montsy744/JSteam",
-    lienSite: "",
-    imgStack: [
-      "./../public/img/stack/html.png",
-      "./../public/img/stack/css.png",
-      "./../public/img/stack/TypeScript.png",
-      "./../public/img/stack/js.png",
-      "./../public/img/stack/git.png",
-    ],
-  },
-  {
-    imgProjet: "./../public/img/project/unniv/fondJsae.png",
-    title: "Warstérïdes",
-    description:
-      "Création d'un jeu shoot'em up en utilisant TS, nodeJs et vite",
-    lienCode: "",
-    lienSite: "",
-    imgStack: [
-      "./../public/img/stack/html.png",
-      "./../public/img/stack/css.png",
-      "./../public/img/stack/TypeScript.png",
-      "./../public/img/stack/git.png",
-      "./../public/img/framework/vite.png",
-    ],
-  },
-  {
-    imgProjet: "./../public/img/project/unniv/fondApiRest.png",
-    title: "API REST",
-    description:
-      "Une API REST développer en Java avec JDBC et JEE sous un server tomcat",
-    lienCode: "https://github.com/Montsy744/SAE_REST_EcoDrop",
-    lienSite: "",
-    imgStack: [
-      "./../public/img/stack/java.png",
-      "./../public/img/stack/git.png",
-    ],
-  },
-];
-
-const renderHtmlStack = (imgStack) => {
+const renderHtmlStack = (imgStack, className) => {
   let htmlTemp = "";
 
   imgStack.forEach((prop) => {
-    htmlTemp += `<img src="${prop}" alt="logo de la Stack"  class="stack"></img>`;
+    htmlTemp += `<img src="${prop}" alt="logo de la Stack"  class="${className}"></img>`;
   });
 
   return htmlTemp;
@@ -374,7 +36,7 @@ const renderHtmlPortfolio = ({ imgProjet, imgStack }) => {
             <div class="imgContainer">
                 <img src="${imgProjet}" alt="Image du lancement du site" class="card__image imgProjet">
                 <div>
-                    ${renderHtmlStack(imgStack)}
+                    ${renderHtmlStack(imgStack, "stack")}
                 </div>
             </div>
         </div>`;
@@ -394,7 +56,7 @@ const renderHtmlPortfolioTel = ({
             <div class="imgContainerTel">
               <img src="${imgProjet}" alt="Image du lancement du site" class="card__image">
               <div>
-                ${renderHtmlStack(imgStack)}
+                ${renderHtmlStack(imgStack, "stack")}
               </div>
             </div>
                         
@@ -450,6 +112,7 @@ projetUnivOrdi.querySelectorAll(".card").forEach((card, index) => {
     e.preventDefault();
     document.querySelector("#mainHeader").classList.add("hide");
     document.querySelector("#containerPortfolio").classList.add("hide");
+    document.querySelector("#descriptionProject").classList.remove("hide");
     document.querySelector("#descriptionProject").classList.add("visible");
     renderHtmlDescriptionView(dataPortfolioUniv[index]);
   });
@@ -459,6 +122,7 @@ const renderHtmlDescriptionView = ({
   imgProjet,
   title,
   description,
+  analyse,
   lienCode,
   lienSite,
   imgStack,
@@ -468,10 +132,44 @@ const renderHtmlDescriptionView = ({
   document.querySelector("#descriptionTitle").innerHTML = `<h1>${title}</h1>`;
   document.querySelector("#imgDescription").innerHTML =
     `<img src="${imgProjet}" alt="Image du projet" />`;
-  document.querySelector("#stackDescription").innerHTML =
-    renderHtmlStack(imgStack);
-  document.querySelector("#contentDescription").innerHTML =
-    `<p>${description}</p>`;
+  document.querySelector("#stackDescription").innerHTML = renderHtmlStack(
+    imgStack,
+    "stackDescription",
+  );
+  document.querySelector("#contentDescription").innerHTML = `
+  <h2> Description </h2>
+  <p class="descriptionText">${description}</p>
+
+  ${
+    analyse
+      ? `
+  <table class="analyseTable">
+    <tbody>
+      <tr>
+        <td class="analyseLabel">Apprentissage</td>
+        <td>${analyse.apprentissage}</td>
+      </tr>
+      <tr>
+        <td class="analyseLabel">Difficultés</td>
+        <td>${analyse.difficultes}</td>
+      </tr>
+      <tr>
+        <td class="analyseLabel">Solutions</td>
+        <td>${analyse.solutions}</td>
+      </tr>
+      <tr>
+        <td class="analyseLabel">Compétences</td>
+        <td>${analyse.competences.map(c => `<span class="badge-competence">${c}</span>`).join("")}</td>
+      </tr>
+      <tr>
+        <td class="analyseLabel">Améliorations</td>
+        <td>${analyse.ameliorations}</td>
+      </tr>
+    </tbody>
+  </table>`
+      : ``
+  }
+`;
   document.querySelector("#btnDescription").innerHTML = liensProjet;
 };
 
